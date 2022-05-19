ba22245b4c3ab678a2ff49be912d1ceb41df99cb
Richard McDonald <rmcd@usgs.gov>
2022-05-19 14:26:04 -0600

    working on readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,10 @@
 """Sphinx configuration."""
+import os
+import sys
 from datetime import datetime
 
+sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("../../"))
 
 project = "Fluvial Particle"
 author = "Richard McDonald"
@@ -12,6 +16,8 @@ extensions = [
     "sphinx_rtd_theme",
     "myst_parser",
 ]
+
+myst_enable_extensions = ["colon_fence"]
 
 source_suffix = {".rst": "restructuredtext", ".md": "markdown"}
 autodoc_typehints = "description"
