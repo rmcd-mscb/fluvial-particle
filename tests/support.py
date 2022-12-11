f02d64ae9a6ae868eff5802b7bfb8b927663180f
Alexander Prescott <alexprescott@email.arizona.edu>
2022-12-11 11:01:29 -0700

    completed docstrings in test support functions

--- tests/support.py
+++ tests/support.py
@@ -9,10 +9,10 @@ def get_h5file(filename: str) -> h5py._hl.files.File:
     """Get and open h5 file.
 
     Args:
-        filename (str): _description_
+        filename (str): path to HDF5 file
 
     Returns:
-        h5py._hl.files.File: _description_
+        h5py._hl.files.File: the opened HDF5 file object
     """
     return h5py.File(filename)
 
@@ -21,10 +21,10 @@ def get_num_timesteps(f: h5py._hl.files.File) -> int:
     """Get number of timesteps.
 
     Args:
-        f (h5py._hl.files.File): _description_
+        f (h5py._hl.files.File): the open HDF5 file object
 
     Returns:
-        int: _description_
+        int: the number of timesteps in the file datasets
     """
     return f["coordinates"]["x"].shape[0]
 
@@ -35,12 +35,13 @@ def get_points(
     """Get point coordinates at time-step time.
 
     Args:
-        f (h5py._hl.files.File): _description_
-        time (int): _description_
-        twod (Optional[bool], optional): _description_. Defaults to False.
+        f (h5py._hl.files.File): the open HDF5 file object
+        time (int): the time index to slice into the datasets
+        twod (Optional[bool], optional): use False to return 2D point coordinates or True to return 3D
+            point coordinates. If 2D, all z values will be set to 0.5. Defaults to False.
 
     Returns:
-        np.ndarray: _description_
+        np.ndarray: point coordinates at the selected time slice
     """
     # f = h5py.File(filename)
     x = f["coordinates"]["x"][time, :]
