5958af9da740eaab1f9189aa19cdb6d516f53dac
Richard McDonald <rmcd@usgs.gov>
2023-02-24 18:04:22 -0700

    added tests in straight channel to avoid boundary conditions in test reproducibility

---
+++ tests/data/user_options_straight_larvaltop.py
@@ -0,0 +1,33 @@
+"""Options file for fluvial particle model."""
+from fluvial_particle.LarvalParticles import LarvalTopParticles
+
+# Paths to 3D and 2D mesh files
+file_name_3d = "./tests/data/Result_straight_3d_1.vtk"
+file_name_2d = "./tests/data/Result_straight_2d_1.vtk"
+
+SimTime = 60.0  # maximum simulation time [seconds]
+dt = 0.25  # simulation time step [seconds]
+PrintAtTick = 20.0  # Print every PrintAtTick seconds
+
+Track3D = 1  # 1 to use 3D velocity field, 0 to use 2D velocity field
+
+NumPart = 20  # Number of particles to simulate per processor
+
+# Starting locations: give tuple for exact point, or path to a checkpoint HDF5 file
+# StartLoc = "tests/test/particles.h5"
+# StartIdx = 45  # optional time slice index to pull data from, defaults to -1
+# StartLoc = (490.0, -4965.0, 530.0)
+StartLoc = (5.0, 0.0, 10.1)
+startfrac = 0.5
+# StartLoc = "./tests/data/varsrc.csv"
+
+# Particle type for the simulation
+ParticleType = LarvalTopParticles  # noqa
+
+# Optional Particles keyword arguments
+beta = (0.067, 0.067, 0.067)  # eddy viscosity coefficient
+lev = 0.00025  # reach-averaged lateral eddy viscosity
+min_depth = 0.02  # minimum depth cells may enter [meters]
+vertbound = 0.01  # depth fraction that bounds particles at bed and water surface
+amp = 0.2
+period = 60.0
---
+++ tests/data/user_options_straight_varsrc.py
@@ -0,0 +1,26 @@
+"""Options file for fluvial particle model."""
+from fluvial_particle.Particles import Particles
+
+# Paths to 3D and 2D mesh files
+file_name_3d = "./tests/data/Result_straight_3d_1.vtk"
+file_name_2d = "./tests/data/Result_straight_2d_1.vtk"
+
+SimTime = 100.0  # maximum simulation time [seconds]
+dt = 0.25  # simulation time step [seconds]
+PrintAtTick = 20.0  # Print every PrintAtTick seconds
+
+Track3D = 1  # 1 to use 3D velocity field, 0 to use 2D velocity field
+
+NumPart = 20  # Number of particles to simulate per processor
+
+# Starting locations: path to a variable start-time CSV
+StartLoc = "./tests/data/varsrc_straight.csv"
+
+# Particle type for the simulation
+ParticleType = Particles  # noqa
+
+# Optional Particles keyword arguments
+beta = (0.067, 0.067, 0.067)  # eddy viscosity coefficient
+lev = 0.00025  # reach-averaged lateral eddy viscosity
+min_depth = 0.02  # minimum depth cells may enter [meters]
+vertbound = 0.01  # depth fraction that bounds particles at bed and water surface
