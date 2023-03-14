8b1cc319a92b2a5bb4f7a5a2a9bf54cd8e50f2eb
Alexander Prescott <alexprescott@email.arizona.edu>
2023-03-14 10:28:25 -0700

    removed meandering channel tests (except npz test)

--- tests/test_main.py
+++ tests/test_main.py
@@ -81,82 +81,6 @@ def run_simulation(argdict: dict) -> None:
             },
             "./tests/data/output_straight_checkpoint",
         ),
-    ],
-)
-def test_particle_straight(argdict: dict, test_out_path: str) -> None:
-    """Test basic particle-tracking."""
-    with TemporaryDirectory() as tmpdirname:
-        argdict["output_directory"] = tmpdirname
-        run_simulation(argdict)
-        # get particle output file
-        new_file = get_h5file(str(argdict.get("output_directory")) + "/particles.h5")
-        new_nts = get_num_timesteps(new_file)
-        new_points = get_points(new_file, new_nts - 1, twod=True)
-        print(type(new_points), new_points.shape)
-        test_file = get_h5file(f"{test_out_path}/particles.h5")
-        test_nts = get_num_timesteps(test_file)
-        test_points = get_points(test_file, test_nts - 1, twod=True)
-
-        assert_allclose(test_points, new_points)
-
-
-@pytest.mark.parametrize(
-    "argdict, test_out_path",
-    [
-        (
-            {
-                "settings_file": "./tests/data/user_options_test.py",
-                "output_directory": "./tests/data/output",
-                "seed": 3654125,
-                "no_postprocess": True,
-            },
-            "./tests/data/output_fixed",
-        ),
-        (
-            {
-                "settings_file": "./tests/data/user_options_falling.py",
-                "output_directory": "./tests/data/output",
-                "seed": 3654125,
-                "no_postprocess": True,
-            },
-            "./tests/data/output_falling_fixed",
-        ),
-        (
-            {
-                "settings_file": "./tests/data/user_options_larvalbot.py",
-                "output_directory": "./tests/data/output",
-                "seed": 3654125,
-                "no_postprocess": True,
-            },
-            "./tests/data/output_larvbot_fixed",
-        ),
-        (
-            {
-                "settings_file": "./tests/data/user_options_larvaltop.py",
-                "output_directory": "./tests/data/output",
-                "seed": 3654125,
-                "no_postprocess": True,
-            },
-            "./tests/data/output_larvtop_fixed",
-        ),
-        (
-            {
-                "settings_file": "./tests/data/user_options_varsrc.py",
-                "output_directory": "./tests/data/output",
-                "seed": 3654125,
-                "no_postprocess": True,
-            },
-            "./tests/data/output_varsrc_fixed",
-        ),
-        (
-            {
-                "settings_file": "./tests/data/user_options_checkpoint.py",
-                "output_directory": "./tests/data/output",
-                "seed": 3654125,
-                "no_postprocess": True,
-            },
-            "./tests/data/output_checkpoint_fixed",
-        ),
         (
             {
                 "settings_file": "./tests/data/user_options_npz.py",
@@ -169,15 +93,15 @@ def test_particle_straight(argdict: dict, test_out_path: str) -> None:
     ],
     ids=(
         "Particles simulation",
+        "simulate with variable start times",
         "FallingParticles simulation",
         "LarvalBotParticles simulation",
         "LarvalTopParticles simulation",
-        "simulate with variable start times",
         "simulate from checkpoint",
         "simulate with npz input meshes",
     ),
 )
-def test_particle(argdict: dict, test_out_path: str) -> None:
+def test_particle_straight(argdict: dict, test_out_path: str) -> None:
     """Test basic particle-tracking."""
     with TemporaryDirectory() as tmpdirname:
         argdict["output_directory"] = tmpdirname
@@ -186,12 +110,11 @@ def test_particle(argdict: dict, test_out_path: str) -> None:
         new_file = get_h5file(str(argdict.get("output_directory")) + "/particles.h5")
         new_nts = get_num_timesteps(new_file)
         new_points = get_points(new_file, new_nts - 1, twod=True)
-        print(type(new_points), new_points.shape)
-        test_file = get_h5file(str(test_out_path) + "/particles.h5")
+        test_file = get_h5file(f"{test_out_path}/particles.h5")
         test_nts = get_num_timesteps(test_file)
         test_points = get_points(test_file, test_nts - 1, twod=True)
 
-        assert_allclose(test_points, new_points)
+        assert_allclose(test_points, new_points, atol=1e-4, rtol=0.0)
 
 
 @pytest.fixture
