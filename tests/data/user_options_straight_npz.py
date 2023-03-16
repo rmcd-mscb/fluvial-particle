00e973b69fdf61e1e24d58d9c7e34bf9ab297c5a
Richard McDonald <rmcd@usgs.gov>
2023-03-16 14:46:42 -0600

    fixed 3d test input file

--- tests/data/user_options_straight_npz.py
+++ tests/data/user_options_straight_npz.py
@@ -2,7 +2,7 @@
 from fluvial_particle.Particles import Particles
 
 # Paths to 3D and 2D mesh files
-file_name_3d = "./tests/data/Result_straight_3d_1.npz"
+file_name_3d = "./tests/data/Result_straight_3d_1_new.npz"
 file_name_2d = "./tests/data/Result_straight_2d_1.npz"
 
 SimTime = 60.0  # maximum simulation time [seconds]
